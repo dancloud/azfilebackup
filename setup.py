3c3cad02ed1069adbba4b196623e17ea156afb12
Pascual <daniel.pascual@sap.com>
2020-09-23 09:47:09 -0500

    Install missing package into setup file

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         ]
     },
     install_requires=[
+        'future>=0.18.2',
         'pid>=2.2.0',
         'azure-storage-common>=1.3.0',
         'azure-storage-blob>=1.3.0,<=2.0.1',
